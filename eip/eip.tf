34830bf0085e085e09e9a711408b641af64f4c05
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2020-03-27 11:00:41 +0100

    eip_update

--- eip/eip.tf
+++ eip/eip.tf
@@ -2,3 +2,7 @@ resource "aws_eip" "myeip" {
   instance = "${aws_instance.my_eip_instance.id}"
   vpc      = true
 }
+
+output "my_eip_address" {  # to output the result in console
+  value = "${aws_eip.myeip}" 
+}
